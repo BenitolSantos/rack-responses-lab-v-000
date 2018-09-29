88685594fb3575e78dc4a3b0bb39aa349ff6fcdc
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-29 22:13:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 require 'pry'
 class Application
   def call(env)
+    #Don't worry about the env input. This holds all of the request info in it and we will use it later!
     #env is the rack environment itself. as shown in the previous lab.
     binding.pry
     #using Time ruby command.
