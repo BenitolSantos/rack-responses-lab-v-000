b0845b20fae7772b38c5c9a136d28a8ff2991779
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-29 22:14:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,11 @@
 require 'pry'
 class Application
   def call(env)
+    resp = Rack::Response.new
     #Don't worry about the env input. This holds all of the request info in it and we will use it later!
     binding.pry
     #using Time ruby command.
+    resp.finish
     if Time.now.hour < 12
       "Good Morning!"
     elsif Time.now.hour > 12
