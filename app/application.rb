d7e8a67c787cf330520770da07927d3fcd59d53f
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-29 22:15:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,11 @@ class Application
     resp = Rack::Response.new
     #Don't worry about the env input. This holds all of the request info in it and we will use it later!
     binding.pry
-    #using Time ruby command.
+
+
     if Time.now.hour < 12
-        resp.write "Good Morning!"
+      #using Time ruby command.
+      resp.write "Good Morning!"
     elsif Time.now.hour > 12
       resp.write "Good Afternoon!"
     else
