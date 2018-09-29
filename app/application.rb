94d8e0dda805d9afccf4335735a49480be07907a
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-29 22:15:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,12 +5,12 @@ class Application
     #Don't worry about the env input. This holds all of the request info in it and we will use it later!
     binding.pry
     #using Time ruby command.
-    resp.finish
     if Time.now.hour < 12
       "Good Morning!"
     elsif Time.now.hour > 12
       "Good Afternoon!"
     else
     end
+    resp.finish
   end
 end
